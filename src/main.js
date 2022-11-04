0e5af9860fc46c761e9685d9ab40c2ab1f8ca1d2
bobo88 <yuanboi88@163.com>
2022-11-04 09:46:05 +0800

    feat: add

--- src/main.js
+++ src/main.js
@@ -4,6 +4,6 @@ import App from "./App.vue";
 import "./assets/main.css";
 
 // var inMainjsNotUsed = 11111;
-console.log("测试 commit msg 4444");
+console.log("测试 commit msg 555");
 
 createApp(App).mount("#app");
