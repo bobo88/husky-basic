1d1404d94977f8742ab7f6f26edc3a5f7703f30f
bobo88 <yuanboi88@163.com>
2022-11-04 09:50:31 +0800

    feat: addddbbbcxx
    gag

--- src/main.js
+++ src/main.js
@@ -4,6 +4,6 @@ import App from "./App.vue";
 import "./assets/main.css";
 
 // var inMainjsNotUsed = 11111;
-console.log("测试 commit msg 555");
+console.log("测试 commit msg 123");
 
 createApp(App).mount("#app");
